0e704194907d2af5de4169b9e40b68f4b06fad00
SONGJIWONPC\SongJiwon <s26788761@naver.com>
2019-09-02 14:51:09 +0900

    add PluginItem.css file & refacforing style code

--- src/PluginItem.js
+++ src/PluginItem.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import "./PluginItem.css";
 
 class PluginItem extends React.Component {
     render(){
         const {name, image, detail, download} = this.props;
         return (
-            <div id="plugin_box" style={{width:"30%", minWidth:"300px", height:"500px", margin:"10px", backgroundColor:"red", textAlign:"center", marginBottom:"20px"}}>
-                <h1 id="plugin_title"style={{textAlign: "center", backgroundColor:"blue"}}id="name">{name}</h1>
-                <img id="plugin_image" src={image} alt={name} width="100%" height="300px"/>
-                <div id="plugin_link" style={{marginTop:"70px", width:"100%", backgroundColor:"yellow", display:"flex", justifyContent:"space-around"}}>
+            <div className="plugin_box">
+                <h1 className="plugin_title" id="name">{name}</h1>
+                <img className="plugin_image" src={image} alt={name}/>
+                <div className="plugin_link">
                     
-                    <div id="plugin_link_detail" style={{textAlign:"left", fontSize:"2.0em", width:"50%", float:"left"}}>
-                        <a id="plugin_detail" href={detail} style={{align: "left"}}>Detail</a>
+                    <div className="plugin_link_detail">
+                        <a href={detail}>Detail</a>
                     </div>
                 
-                    <div id="plugin_link_download" style={{textAlign:"right", fontSize:"2.0em", width:"50%", float:"right"}}>
-                        <a id="plugin_detail" href={download} style={{align: "right"}}>Download</a>
+                    <div className="plugin_link_download">
+                        <a href={download}>Download</a>
                     </div>
-
-    
                     
                 </div>
             </div>
